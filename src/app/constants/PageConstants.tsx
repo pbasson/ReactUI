08b7d8a3d8aee39a717ffa9608e9e39a83e3246a
preetpal basson <preet.basson@gmail.com>
2026-03-11 11:35:44 +0000

    0.08: Update

--- src/app/constants/PageConstants.tsx
+++
@@ -1,7 +0,0 @@
-export class PageConstants {
-  static readonly spinnerText = "Loading...";
-  static readonly brandName = "ReactUI";
-  static readonly headerText = "Welcome To React Testing";
-  static readonly systemUser = "Preetpal Basson";
-  static readonly refresh = "Refresh";
-}
